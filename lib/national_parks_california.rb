6f537187452d31a59b6841a567b9e3fa4feea789
Nory Lazo <nlazo@me.com>
2020-01-15 20:49:12 -0800

    Set up Ruby Gem environment and files.

--- lib/national_parks_california.rb
+++ lib/national_parks_california.rb
@@ -1,4 +1,7 @@
-require "national_parks_california/version"
+require_relative "./national_parks_california/version"
+require_relative "./national_parks_california/park.rb"
+require_relative "./national_parks_california/scraper.rb"
+require_relative "./national_parks_california/cli.rb"
 
 module NationalParksCalifornia
   class Error < StandardError; end
---
+++ lib/national_parks_california/cli.rb
@@ -0,0 +1,6 @@
+class NationalParksCalifornia::CLI
+
+    def call 
+        puts "Hello world."
+    end 
+end 
