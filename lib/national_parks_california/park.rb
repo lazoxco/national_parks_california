b1bcfb5988c1017b52e913ea28913cae9eefbe74
Nory Lazo <nlazo@me.com>
2020-01-15 20:57:17 -0800

    Created Park object.

--- lib/national_parks_california/park.rb
+++ lib/national_parks_california/park.rb
@@ -1,3 +1,19 @@
 class Park
 
+    attr_accessor :name, :type, :location, :description
+
+    @@all = []
+
+    def initialize(park_arrtibutes)
+        @name = park_arrtibutes[:name]
+        @type = park_arrtibutes[:type]
+        @location = park_arrtibutes[:location]
+        @description = park_attributes[:description]
+
+        @@all << self
+    end
+
+    def self.all
+        @@all
+    end 
 end 
